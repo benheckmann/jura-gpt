9599f8f22398ed280c43489013c27bcab4484510
Ben Heckmann <ben.c.heckmann@gmail.com>
2023-05-23 14:00:57 +0200

    improved xml parsing

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import re
 from typing import Union, Dict, Any, List, Optional, Literal
 from typing_extensions import TypedDict
 import xml.etree.ElementTree as ET
@@ -5,6 +6,38 @@ import pandas as pd
 import numpy as np
 
 from haystack import Document
+
+def parse_element(element):
+    """ Recursively parses an XML element and returns a dictionary of its structure """
+    parsed_element = {}
+    for child in element:
+        if len(child) > 0:  # element has child elements
+            parsed_element[child.tag] = parse_element(child)
+        else:
+            parsed_element[child.tag] = child.text
+    return parsed_element
+
+def parse_content(element):
+    """ Recursively parses an XML content structure and returns a string of the contents """
+    content = []
+    for child in element:
+        # If there is text directly within this child, append it
+        if child.text is not None and child.text.strip() != '':
+            content.append(child.text.strip())
+        # If this child is a BR tag, append a newline
+        if child.tag == 'BR':
+            content.append('\n')
+        # If there are further child elements, recursively parse those
+        elif len(child) > 0:
+            content.append(parse_content(child))
+        # If there is text after this child element (tail), append that
+        if child.tail is not None and child.tail.strip() != '':
+            content.append(child.tail.strip())
+    content_string = " ".join(content)
+    # replace all instances of newline followed by multiple spaces with a single newline
+    content_string = re.sub("\n\s+", "\n", content_string)
+    return content_string
+
 
 def parse_gg_xml_to_documents(xml_file: str) -> List[Document]:
     tree = ET.parse(xml_file)
@@ -19,17 +52,16 @@ def parse_gg_xml_to_documents(xml_file: str) -> List[Document]:
         # Extract metadata
         meta = {}
         if metadaten is not None:
-            for child in metadaten:
-                meta[child.tag] = child.text
+            meta = parse_element(metadaten)
 
         # Extract content
         content = ""
         if textdaten is not None:
             text = textdaten.find("text")
             if text is not None:
-                content = ET.tostring(text, encoding="utf-8", method="text").decode(
-                    "utf-8"
-                )
+                content_element = text.find("Content")
+                if content_element is not None:
+                    content = parse_content(content_element)
 
         # Create document
         document = Document(
@@ -41,7 +73,6 @@ def parse_gg_xml_to_documents(xml_file: str) -> List[Document]:
         documents.append(document)
 
     return documents
-
 
 _xml_file = "./data/grundgesetz.xml"
 documents = parse_gg_xml_to_documents(_xml_file)
